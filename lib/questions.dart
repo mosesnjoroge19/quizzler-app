1a4e485c7cac87ae05c217b25257d3123902c854
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-05-12 22:25:25 +0300

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,11 @@
+class Question{
+  //Question's properties
+  String questionText;
+  bool questionanswer;
+
+  //Constructor to set values for properties in a new object
+  Question({String q, bool a}){
+    questionText = q;
+    questionanswer = a;
+  }
+}
